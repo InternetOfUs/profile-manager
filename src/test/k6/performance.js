a910ca215cff4d08a10cd9fcd5b57ca68989da85
Bruno Rosell i Gui (UDT-IA, IIIA-CSIC) <rosell@localhost>
2020-11-11 18:27:21 +0100

    Do the test on groups.

--- src/test/k6/performance.js
+++ src/test/k6/performance.js
@@ -25,7 +25,7 @@
  */
 
 import http from 'k6/http';
-import { check } from 'k6';
+import { group, check } from 'k6';
 
 /**
  * Test to check the performance of the profile manager.
@@ -37,34 +37,86 @@ export default function() {
 
 		profileManagerApi = __ENV.PROFILE_MANAGER_API;
 	}
-	var createPayload = JSON.stringify({ name: { prefix: 'k6', first: 'performance', last: 'test' } });
-	var createParams = {
-		headers: {
-			'Content-Type': 'application/json',
-		},
-	};
-	var createResponse = http.post(profileManagerApi + '/profiles', createPayload, createParams);
-	check(createResponse, {
-		'created profile': (r) => r.status === 201,
-	});
-	var profile = createResponse.json();
-	var retrieveResponse = http.get(profileManagerApi + '/profiles/' + profile.id);
-	check(retrieveResponse, {
-		'retrieved profile': (r) => r.status === 200,
-		'expected profile': (r) => r.body == createResponse.body,
-	});
-	var retrievePageResponse = http.get(profileManagerApi + '/profiles');
-	check(retrievePageResponse, {
-		'retrieved page': (r) => r.status === 200
-	});
-	var deleteResponse = http.del(profileManagerApi + '/profiles/' + profile.id);
-	check(deleteResponse, {
-		'deleted profile': (r) => r.status === 204,
-	});
-	var noRetrieveResponse = http.get(profileManagerApi + '/profiles/' + profile.id);
-	check(noRetrieveResponse, {
-		'no retrieved profile': (r) => r.status === 404,
-	});
+	group('profile manager performance', function() {
 
+		let profile;
+		group('create profile', function() {
+
+			var profileToCreate = {
+				name: {
+					prefix: 'k6',
+					first: 'performance',
+					last: 'test'
+				}
+			};
+			var createPayload = JSON.stringify(profileToCreate);
+			var createParams = {
+				headers: {
+					'Content-Type': 'application/json',
+				}
+			};
+			var createResponse = http.post(profileManagerApi + '/profiles', createPayload, createParams);
+			check(createResponse, {
+				'created profile': (r) => r.status === 201,
+				'obtain created profile': (r) => {
+					profile = r.json();
+					return profile !== undefined;
+				}
+			});
+		});
+
+		group('retrieve profile', function() {
+
+			var retrieveResponse = http.get(profileManagerApi + '/profiles/' + profile.id);
+			check(retrieveResponse, {
+				'retrieved profile': (r) => r.status === 200,
+				'validate profile': (r) => {
+					var received = JSON.stringify(r.json()).split('').sort().join('');
+					var expected = JSON.stringify(profile).split('').sort().join('');
+					return received == expected;
+				}
+			});
+			var retrievePageResponse = http.get(profileManagerApi + '/profiles');
+			check(retrievePageResponse, {
+				'retrieved page': (r) => r.status === 200,
+				'validate page': (r) => {
+
+					var page = r.json();
+					if (page.total == 0) {
+
+						return false;
+
+					} else if ( page.total > page.profiles.length){
+
+						return true;
+
+					} else {
+
+						var expected = JSON.stringify(profile).split('').sort().join('');
+						for (var i = page.profiles.length - 1; i >= 0; i--) {
+
+							var received = JSON.stringify(page.profiles[i]).split('').sort().join('');
+							if (received == expected) {
+
+								return true;
+							}
+
+						}
+
+						return false;
+					}
+				}
+			});
+
+		});
+
+		group('delete profile', function() {
+
+			var deleteResponse = http.del(profileManagerApi + '/profiles/' + profile.id);
+			check(deleteResponse, {
+				'deleted profile': (r) => r.status === 204
+			});
+		});
+	});
 
 }
