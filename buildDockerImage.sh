5284bdf2760f7512ceff4e2c62712ca054c2afd3
Bruno Rosell i Gui <rosell@iiia.csic.es>
2021-10-21 11:07:31 +0200

    Allow to create image using docker nexus maven package manager

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -8,6 +8,7 @@ else
 	COMPONENT_VERSION=$(grep -m1 '<version>' pom.xml |cut -d '<' -f2  |cut -d '>' -f2)
 	COMPONENT_NAME="internetofus/profile-manager"
 	DOCKER_TAG="$COMPONENT_NAME:$COMPONENT_VERSION"
-	DOCKER_BUILDKIT=1 docker build -f src/main/docker/Dockerfile -t $DOCKER_TAG .
+	PROFILE=${1:-"gitlab"}
+	DOCKER_BUILDKIT=1 docker build --build-arg DEFAULT_PROFILE=$PROFILE -f src/main/docker/Dockerfile -t $DOCKER_TAG .
 	popd >/dev/null
 fi
